ce91867de561f6298ca4087ec1b61a79d03dee64
programapedro <pedrohugo.ov09@gmail.com>
2018-12-09 04:47:17 -0200

    Validação funcionado

--- src/app/form-adm/form-adm.component.ts
+++ src/app/form-adm/form-adm.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {SelectItem} from 'primeng/primeng';
+import { SelectItem } from 'primeng/primeng';
 import { Router, Route } from '@angular/router';
-import {PanelModule} from 'primeng/primeng';
-import {ContextMenuModule} from 'primeng/primeng';
-import {MenuItem} from 'primeng/primeng';
+import { PanelModule } from 'primeng/primeng';
+import { ContextMenuModule } from 'primeng/primeng';
+import { MenuItem } from 'primeng/primeng';
+import { UserService } from '../user.service';
+import { TabMenuModule } from 'primeng/primeng';
+import { RadioButtonModule } from 'primeng/primeng';
+import { Message } from 'primeng/primeng';
+import { User } from '../models/user'
+import {Validators,FormControl,FormGroup,FormBuilder} from '@angular/forms';
 
 
 @Component({
@@ -12,10 +18,10 @@ import {MenuItem} from 'primeng/primeng';
   styleUrls: ['./form-adm.component.css']
 })
 export class FormAdmComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  
+      constructor() {}
+  
+      ngOnInit() {
+      }
 
 }
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user'
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
-import {TabMenuModule, MenuItem} from 'primeng/primeng';
-import {RadioButtonModule} from 'primeng/primeng';
-import {Message} from 'primeng/primeng';
+import { TabMenuModule, MenuItem } from 'primeng/primeng';
+import { RadioButtonModule } from 'primeng/primeng';
+import { Message } from 'primeng/primeng';
 
 
 @Component({
@@ -19,85 +19,41 @@ export class FormularioComponent implements OnInit {
   senhaUsuario: string;
 
   msgs: Message[] = [];
-  //selectedValue: string = "val1";
-  
-  constructor(private userService: UserService,private rota: Router) { 
- }
-    /*loginUsuario(user, senha){
-      user = this.userUsuario;
-      senha = this.senhaUsuario;
-      this.userService.loginUsuario(user, senha).subscribe(usuario => {
-        if(user == null){
-          alert("Usuário não cadastrado no banco.")
-        }else if(user == "" && senha == "") {
-          console.log("Preencha os campos de usuário e senha!");
-        } else{
-          console.log("Usuario logado: "+user );
-          this.userService.usuarioLogado = user; 
-          this.rota.navigate(['/main']); 
 
-
-        }
-      });
-    } */
-
-    showSuccess() {
-      this.msgs = [];
-      this.msgs.push({severity:'success', summary:'Login feito com sucesso', detail:'Usuario Logado'});
+  constructor(private userService: UserService, private rota: Router) {
   }
 
-    login(user, senha){
-      user = this.userUsuario;
-      senha = this.senhaUsuario;
-      
-      this.userService.loginUsuario(user, senha).subscribe(meuObservable => {
-        if(meuObservable == null){
-          alert("Usuário não cadastrado no banco.")
-        } else{
-          this.rota.navigate(['/main/home']);
+  showError() {
+    this.msgs = [];
+    this.msgs.push({ severity: 'error', summary: 'Usuário/Senha inválido', detail: 'Verique as informações e tente novamente.' });
+  }
+  showInfo() {
+    this.msgs = [];
+    this.msgs.push({ severity: 'info', summary: 'Bem-vindo ao HEAZY', detail: 'Seja bem-vindo!' });
+  }
 
-          this.userService.loginUsuario(user, senha).subscribe(meuObservable => {
-            
-         // console.log(meuObservable)
-        
+  login(user, senha) {
+    user = this.userUsuario;
+    senha = this.senhaUsuario;
+
+    this.userService.loginUsuario(user, senha).subscribe(meuObservable => {
+      if (meuObservable == null) {
+        this.showError();
+      } else {
+        this.rota.navigate(['/main/home']);
+        this.userService.loginUsuario(user, senha).subscribe(meuObservable => {
           this.userService.usuarioLogado = meuObservable as User
-         console.log("Usuario Logado: " + this.userService.usuarioLogado.username)
+          console.log("Usuario Logado: " + this.userService.usuarioLogado.username)
         })
       }
-     });
-    }        
-    /**  if(user == this.userUsuario && senha == this.senhaUsuario){
-        this.rota.navigate(['/main'])
-       } else {
-          console.log("usuário não existe");
-        }
-      }
-     */
+    });
+  }
 
-    signInWithGoogle() {
-      this.userService.loginGoogle()
-        .then(() => {
-          this.rota.navigate(['/main']);
-        })
-        .catch((err) => console.log(err));
-    }
-
-    irParaAdministracao(){
-      this.rota.navigate(['/adm-menu/listar-users']);
-    }
+  irParaAdministracao() {
+    this.rota.navigate(['/adm-menu/listar-users']);
+  }
 
   ngOnInit() {
-  //this.users = this.userService.getUsers();  
 
   }
-  /*entrar(user:User){
-   let podeLogar:boolean = false;
-   podeLogar = this.userService.usuarioExiste(this.user);
-   if(podeLogar){
-     this.rota.navigate(["main"])
-   }else{
-     alert("Usuário não encontrado, faça seu cadastro")
-   }
- } */
-
 }
